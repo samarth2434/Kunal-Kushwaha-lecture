23d486b4dafdd82c7d114d1c0b20289b5e783573
samarth2434 <140902962+samarth2434@users.noreply.github.com>
2025-01-10 22:35:26 +0530

    main

--- lectureLL/LL.java
+++ lectureLL/LL.java
@@ -1,31 +1,73 @@
-public class LL{
+public class LL {
 
-    //every linked list have head and tail 
-    //head and tail are the refernece var point to the node 
+    // Every linked list has head and tail
+    // Head and tail are reference variables pointing to nodes
     private Node head;
     private Node tail;
-    
+
     private int size;
-    public LL(){
-        this.size = 0;  
+
+    public LL() {
+        this.size = 0;
     }
-    
-    //insertion in singly LL
-    public void insertFirst(int val){
-        //create a new node 
+
+    // Insertion in singly linked list
+    public void insertFirst(int val) {
+        // Create a new node
         Node node = new Node(val);
         node.next = head;
         head = node;
 
-        if(tail == null){
+        if (tail == null) {
             tail = head;
         }
         size += 1;
+    }
+    //insert in the last index 
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+            return; 
+        }
+        //create a new node 
+        Node node = new Node(val);
+        tail.next = node; //add the new node or point to the node 
+        tail = node;   //jo val add kari hai usko he tail point karege 
+        size++;
 
     }
+    //insert at the particular index 
+    public void insert(int val, int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if(index == size){
+            insertLast(val);
+            return;
+        }
 
-    //Representation 
-    private class Node{
+        //now check and go 
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+        size++;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    // Representation
+    private class Node {
         private int value;
         private Node next;
 
@@ -33,9 +75,22 @@ public class LL{
             this.value = value;
         }
 
-        public Node(int value, Node next){
+        public Node(int value, Node next) {
             this.value = value;
             this.next = next;
         }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(100, 2);
+
+        list.display();
     }
 }
--- lectureLL/main.java
+++ lectureLL/main.java
@@ -1,5 +1,12 @@
 public class main{
     public static void main(String[] args) {
         LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+
+        list.display();
+
     }
 }
